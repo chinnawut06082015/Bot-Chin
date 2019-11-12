23326016f507f3d6da251db4d87fcf06134dd016
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-12 16:31:14 +0700

    re

--- bot.php
+++ bot.php
@@ -1,8 +1,6 @@
 <?php
  http_response_code(200);
- 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);
+
 
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
@@ -20,6 +18,10 @@ if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
   $inputData = file_get_contents("php://input");
 
 //LINEBOTSDKの設定
+ 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);
+
   $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
   $Events = $Bot->parseEventRequest($InputData, $Signature);
 
