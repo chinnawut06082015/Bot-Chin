1521a3b5e0e5d4c9c870173b93b8202ba3f83cfb
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-12 16:20:14 +0700

    test

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-require "vendor/autoload.php";
+require __DIR__ . '/../vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);
